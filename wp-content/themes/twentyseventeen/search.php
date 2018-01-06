b6209c0560955f79c0d0a236eac7be10727a1198
Nanja <steph.o-91@hotmail.com>
2018-01-06 14:40:40 -0800

    Updated the search results page and limited the search terms to the blog

--- wp-content/themes/twentyseventeen/search.php
+++ wp-content/themes/twentyseventeen/search.php
@@ -9,54 +9,87 @@
  * @since 1.0
  * @version 1.0
  */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-get_header(); ?>
+<?php wp_head(); ?>
+</head>
 
-<div class="wrap">
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
 
-	<header class="page-header">
-		<?php if ( have_posts() ) : ?>
-			<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyseventeen' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-		<?php else : ?>
-			<h1 class="page-title"><?php _e( 'Nothing Found', 'twentyseventeen' ); ?></h1>
+	<header id="masthead" class="site-header" role="banner">
+		<?php if ( has_nav_menu( 'top' ) ) : ?>
+			<div class="navbar">
+				<div class="wrap">
+					<h1 class="banner-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Warren Olfert</a></h1>
+					<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
+				</div><!-- .wrap -->
+			</div><!-- .navigation-top -->
 		<?php endif; ?>
-	</header><!-- .page-header -->
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<div class="banner-wrap">
+			<div class="banner-image" style="background-image: url(<?php the_field('banner_image'); ?>);"></div>
 
-		<?php
-		if ( have_posts() ) :
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			<div class="banner-details">
+				<h1 class="banner-title">Search Results</h1>
+				<div class="red-underline"></div>
+				<p class="banner-subtitle"><?php the_field('page_subtitle') ?></p>
+			</div>
+		</div>
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/post/content', 'excerpt' );
+	</header><!-- #masthead -->
 
-			endwhile; // End of the loop.
+	<div class="site-content-contain">
+		<div id="content" class="site-content">
 
-			the_posts_pagination( array(
-				'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-				'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-			) );
+				<header class="page-header search-header">
+					<?php if ( have_posts() ) : ?>
+						<h1 class="page-title"><?php printf( __( 'Search Results for "%s', 'twentyseventeen' ), '<span>' . get_search_query() . '"</span>' ); ?></h1>
+					<?php else : ?>
+						<h1 class="page-title"><?php _e( 'Nothing Found', 'twentyseventeen' ); ?></h1>
+					<?php endif; ?>
+				</header><!-- .page-header -->
 
-		else : ?>
+				<div id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
 
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentyseventeen' ); ?></p>
-			<?php
-				get_search_form();
+					<?php
+					if ( have_posts() ) :
+						/* Start the Loop */
+						while ( have_posts() ) : the_post();
+							?>
+							<div class="thoughts-link-wrap">
+								<?php
+								the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>');?>
+								<p><?php the_field('blurb')?></p>
+							</div><?php
 
-		endif;
-		?>
+						endwhile; // End of the loop.
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
-</div><!-- .wrap -->
+						the_posts_pagination( array(
+							'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+							'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+							'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+						) );
 
-<?php get_footer();
+					else : ?>
+
+						<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentyseventeen' ); ?></p>
+						<?php
+							get_search_form();
+
+					endif;
+					?>
+
+					</main><!-- #main -->
+				</div><!-- #primary -->
+				<?php get_sidebar(); ?>
+
+			<?php get_footer();
