d3e8f4d676579a97a08bd9b3a3fde88650121e50
Nanja <steph.o-91@hotmail.com>
2018-01-07 12:15:44 -0800

    Added responsive views for up to 768px viewport

--- wp-content/themes/twentyseventeen/index.php
+++ wp-content/themes/twentyseventeen/index.php
@@ -53,14 +53,6 @@ wp_head(); ?>
 	<header id="landing-header-wrap" role="banner">
 		<nav id="landing-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentyseventeen' ); ?>">
 			<i class="material-icons close-navigation no-desktop">clear</i>
-			<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
-				<?php
-				echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) );
-				echo twentyseventeen_get_svg( array( 'icon' => 'close' ) );
-				_e( 'Menu', 'twentyseventeen' );
-				?>
-			</button>
-
 			<?php wp_nav_menu( array(
 				'theme_location' => 'top',
 				'menu_id'        => 'top-menu',
